0f293c1f6d5e5aac6e3556636a553c109e1c83b2
Sergio Jimenez <sj14012004@gmail.com>
2025-05-18 23:24:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,6 @@ def detalle_por_path(id_mision):
     return render_template('mision.html', mision=m)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
